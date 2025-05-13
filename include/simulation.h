3f101f877f31e738e1094ed4b14ada137fdd337e
vishali-u <vumaiyalan@gmail.com>
2025-05-13 19:26:34 -0400

    add results storage

--- include/simulation.h
+++ include/simulation.h
@@ -12,11 +12,21 @@ typedef struct {
     double selection_coefficient; // Selection coefficient for the allele
 } SimParameters;
 
+typedef struct {
+    int replicate; // Replicate number
+    int population_size; // Population size
+    int generation; // Generation number
+    int initial_allele_frequency; // Initial allele frequency
+    int final_allele_frequency; // Final allele frequency
+} SimResults;
+
 /*
  * Run the Wright-Fisher simulation with the given parameters.
  * Writes the results to a specified output file.
 */
 void run_simulation(SimParameters params);
+
+
 
 
 
--- src/simulation.c
+++ src/simulation.c
@@ -3,7 +3,11 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
-void runSimulation(SimParameters params) {
+/*
+* Run the Wright-Fisher simulation with the given parameters.
+* Return a pointer to the results structure.
+*/
+SimResults *runSimulation(SimParameters params) {
 
     // Initialize random number generator (RNG)
     // Use the Mersenne Twister algorithm 
@@ -13,16 +17,41 @@ void runSimulation(SimParameters params) {
     int N = params.population_size;
     double p;
 
+    // Initialize an array to store the results of each replicate
+    SimResults *results = malloc(params.num_reps * params.generations * sizeof(SimResults));
+
+    if (results == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
     // TODO: Write each replicate result to a file
     // Run the simulations for the given number of replicates
+    int replicate_index = 0;
     for (int rep = 0; rep < params.num_reps; rep++) {
         p = params.initial_allele_frequency;
+        SimResults result;
 
         for (int gen = 0; gen < params.generations; gen++) {
             unsigned int X = gsl_ran_binomial(r, p, (2 * N));
             double p = (double) X / (2 * N);
+
+            // Store results of current generation
+            result.replicate = rep;
+            result.population_size = N;
+            result.generation = gen;
+            result.initial_allele_frequency = params.initial_allele_frequency;
+            result.final_allele_frequency = p;
+            results[replicate_index] = result;
+            replicate_index++;
         }
     }
 
     gsl_rng_free(r); 
+
+    return results, params.num_reps * params.generations;
+}
+
+void writeResultsToFile(SimResults *results, int num_results) {
+    
 }
